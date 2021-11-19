e3e95f7d37c13ace8355f043844b24fbf027dc1c
Emi1213 <emi1213galarza@gmail.com>
2021-11-19 09:29:08 -0500

    scooby

---
+++ src/dataTypes .js
@@ -0,0 +1,13 @@
+// Numbers
+X = 2;
+y = 3;
+x = 4;
+// Strings o Cadenas de Texto
+name = 'Emilia';
+lastname = 'Galarza';
+// Arreglos, Arrays, Vectores
+numbers = [1, 2, 3, 4, 5];
+vowels = ['a', 'e', 'i', 'o', 'u'];
+arregloVacio = [];
+
+// para arbir la consola ctrl + shift + ñ
